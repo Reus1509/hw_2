32c818cd396217c5138f32c8903969ceba32d0d9
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-12-19 16:03:12 +0300

    change from sort to shuffle

--- task_5.py
+++ task_5.py
@@ -1,19 +1,9 @@
 # Реализуйте алгоритм перемешивания списка.
 
 import os
+import random
 os.system('cls')
 
-
-def sort(nums):
-    sorted = True
-    while sorted:
-        sorted = False
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i+1]:
-                nums[i], nums[i+1] = nums[i+1], nums[i]
-                sorted = True
-
-
-nums = [5, 3, 7, 1, 9]
-sort(nums)
+nums = [3, 1, 5, 3, 9]
+random.shuffle(nums) #Поперемешиваем, можно без сортировки)
 print(nums)
